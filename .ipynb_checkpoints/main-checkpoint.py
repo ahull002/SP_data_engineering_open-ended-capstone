7af91fdfc83b7db7c3c24379fb1384935dbc2d24
alfred hull <alfred_d_hull@outlook.com>
2020-12-18 19:06:46 -0500

    sample data file update and main.py update

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -1,88 +1,101 @@
-# Import libraries
 import os
-import urllib.request
 import datetime
+import urllib.request
 
-# get currrent year, moonth
-today = datetime.datetime.now()
 
-# Initiate the program with a welcome message
-print(f' --------------------------------------------\n Starting Program on: {today.year}-{today.month}-{today.day}\n --------------------------------------------\n')
+FILENAMES = [
+    "sample_yellow_tripdata.csv",
+    "sample_green_tripdata.csv",
+    "sample_fhv_tripdata.csv",
+    "sample_fhvhv_tripdata.csv",
+]
 
-# Supply the target/base URLs that will be scrapped for this project
-base_url = ['https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_',
-            'https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_',
-            'https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_',
-            'https://nyc-tlc.s3.amazonaws.com/trip+data/fhvhv_tripdata_']
+BASE_URLS = [
+    "https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_",
+    "https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_",
+    "https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_",
+    "https://nyc-tlc.s3.amazonaws.com/trip+data/fhvhv_tripdata_",
+]
 
-# Provide file names that will be used to house data
-filenames = ["tf_yellow_tripdata.csv", "tf_green_tripdata.csv", "tf_fhv_tripdata.csv",  "tf_fhvhv_tripdata.csv"]
+SUCCESS_LOG = "success_files"
 
-# Create a function to:
-def appendData(year, month):
-    
-    for i in range(4):
-        url = base_url[i] + str(year) + "-" + '{0:02d}'.format(month) + ".csv" # concatenate base_yrl + year + month + .csv, to create web links: url        
-            
-        
-        # Build try clause to: check if file path exists if not write one if so (else) open the appropriate file and append data
+SAMPLING = True
+SAMPLE_LINES = 2
+
+
+def _get_project_url(base_url, month, year):
+    formatted_month = "{0:02d}".format(month)
+    return f"{base_url}{year}-{formatted_month}.csv"
+
+
+def _process_url(url, filename):
+    print(f"Starting URL processing for {url} and {filename}")
+    datasource = urllib.request.urlopen(url)
+
+    file_exists = os.path.isfile(filename)
+    write_mode = "a" if file_exists else "w"
+    with open(filename, write_mode) as f, open(SUCCESS_LOG, "a") as success_f:
+        lines_from_datasource = [datasource.readline() for i in range(SAMPLE_LINES)]
+        for i, line in enumerate(lines_from_datasource):
+            if not line:
+                continue
+
+            if i == 0 and file_exists:
+                # if downloaded file exists skip header
+                # as you already have it from previous file
+                continue
+
+            f.write(line.decode("utf8").rstrip())
+           
+            if SAMPLING and i > SAMPLE_LINES:
+                break
+        success_f.write(f"{url}\n")
+
+
+def _print_neat_error(err, month, year, url):
+    print(
+        f"""
+        {err}\nThe above error occured for the following:\n
+        URL: {url}
+        Month: {month}
+        Year: {year}
+        {_line_separator()}
+    """
+    )
+
+
+def sync_files(month, year):
+    urls = [_get_project_url(base_url, month, year) for base_url in BASE_URLS]
+    for url, file in zip(urls, FILENAMES):
         try:
-            datasource = urllib.request.urlopen(url) # access urls and open a stream to download data from them
-            if not os.path.isfile(filenames[i]):
-                fp = open(filenames[i], "w")
-                new_file = True # indicate if open new file
-            else:  # else it exists so append without writing the header (to prevent overwriting data within source file)
-                fp = open(filenames[i], "a")
-                new_file = False # indicate that open existing file
-            count = 0
-            
-            while True:
-                line = datasource.readline()
-                if new_file == False and count == 0: # if downloaded file exists skip header as you already have it from previous file 
-                    count += 1
-                    continue
-
-                if not line: # check if .csv stream is ended or not
-                    break
-
-                line = line.decode("utf-8").rstrip() # convert binary value to string and remove the last return character('\r\n')
-
-                if not line: # skip empty line
-                    continue
-
-                line = line + '\n'
-                fp.write(line)
-                count += 1
-
-                if count % 50000 == 0:
-                    print("Line " + str(count) + " is processed")
-
-                if count > 2: # just save 1000 records for each csv file for quick testing
-                    break
-
-            fp.close()
-            
-            print(url + " is Okay\n")    
+            _process_url(url, file)
         except Exception as e:
-            print(f' --- The following: -{str(e)}- has occured for the following filles:')
-            # print(f'Year: {str(year)} Month = {str(month)}')
-            print(url + " is Invalid")    
-            print(f'========================================================================')
+            _print_neat_error(e, month, year, url)
 
 
-# Write files
-# Extract all of the historocal .csv data and merge except for this month
-print(f' --------------------------------------------\n Whole Data Overview\n --------------------------------------------\n')
-stop_flag = False
-for year in range(2009, today.year + 1):
-    if stop_flag == True:
-        break
-        
-    for month in range(1, 13):
-        if year == today.year and month == today.month:
-            stop_flag = True
-            break
+def _line_separator():
+    return "=" * 50
+
+
+def _banner():
+    return "%s\nWhole Data Overview\n%s" % ((_line_separator(),) * 2)
+
+
+def _greeting(day, month, year):
+    return f"Starting program for {month}-{year}-{day}\n" + _line_separator()
+
+
+def run():
+    today = datetime.datetime.now()
+    current_day, current_month, current_year = today.day, today.month, today.year
+
+    print(_greeting(current_day, current_month, current_year))
+    print(_banner())
+
+    for year in range(2009, current_year + 1):
+        for month in range(1, 13):
+            if (month, year) == (current_month, current_year):
+                return
+            sync_files(month, year)
             
-        appendData(year, month)
-        
-#delta
+run()
